fab885b49161535c1885764e880c33d6377b32e3
Mike Harrison <mharrisonutcoding@gmail.com>
2022-09-01 23:37:05 -0500

    updated criteria for time blocks
    updated the criteria for the class changes for the colors using

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,5 +22,24 @@ $(document).ready(function () {
         // loop over time blocks
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
             
+            // To check the time and add the classes for background indicators
+            
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
 })
