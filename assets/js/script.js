07fabcbbf4738bec99dea604d993a3e72eedeea8
Mike Harrison <mharrisonutcoding@gmail.com>
2022-09-01 23:32:27 -0500

    updated the js file
    added a click listener function to save user input to the local storage, as well as  a time tracker for changing the status color of the time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,26 @@
+// Displays todays date and the day
+
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
 
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+
+    function timeTracker() {
+        //get current number of hours
+        var timeNow = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            
+})
